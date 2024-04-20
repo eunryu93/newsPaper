f0072ded8387877b9570bb5ebcba9691b3c63583
은냥 <cutely93@gmail.com>
2024-04-20 22:12:46 +0900

    init Project
    init Project

---
+++ NewsPaper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsPaper
+//
+//  Created by 은냥 on 4/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
